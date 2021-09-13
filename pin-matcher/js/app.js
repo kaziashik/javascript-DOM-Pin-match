30454e8bfc1868a7ad1db839fb7e837cdfa54cde
Kazi Ashikur Rahman <64448137+kaziashik@users.noreply.github.com>
2021-09-13 17:19:21 +0800

    javascript-DOM-Pin-match
    uplod javascript-DOM-Pin-match

---
+++ pin-matcher/js/app.js
@@ -0,0 +1,71 @@
+function getPin() {
+    const pin = Math.round(Math.random() * 10000);
+    const pinString = pin + '';
+    if (pinString.length == 4) {
+        return pin;
+    }
+
+    else {
+        // console.log('got 3 digit and all getpin',pin);
+        return getPin();
+    }
+}
+
+function generatePin() {
+    const pin = getPin();
+    document.getElementById('display-pin').value = pin;
+    // document.getElementById('test').value = pin;
+}
+
+
+document.getElementById('key-pad').addEventListener('click', function (event) {
+
+    const number = event.target.innerText;
+    const calcInput = document.getElementById('typed-numbers');
+    if (isNaN(number)) {
+
+        if (number == 'C') {
+            calcInput.value = '';
+        }
+    }
+
+    else {
+
+        const previouscalcInput = calcInput.value;
+        const newcalcInput = previouscalcInput + number;
+        calcInput.value = newcalcInput;
+    }
+
+});
+
+
+
+function verifyPin() {
+    const pin = document.getElementById('display-pin').value;
+
+    const typedNumbers = document.getElementById('typed-numbers').value;
+
+    const successMessage = document.getElementById('notify-success');
+
+    const failError = document.getElementById('notify-faild');
+
+
+    if (pin == typedNumbers) {
+
+        successMessage.style.display = 'block';
+        failError.style.display = 'none'
+
+
+
+
+
+
+    }
+
+    else {
+
+
+        failError.style.display = 'block'
+        successMessage.style.display = 'none';
+    }
+}
